ee36431febef24e6ea651899c34a9a4840ecc7ac
paulvinzent <manalopv@gmail.com>
2023-10-14 20:35:31 +0800

    import css

--- src/views/Electro.vue
+++ src/views/Electro.vue
@@ -20,5 +20,14 @@
     }
 </script>
 <style>
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,700');
+    @import 'slick-carousel/slick/slick.css';
+    @import 'slick-carousel/slick/slick-theme.css';
+    @import '@/assets/electro/css/nouislider.min.css';
+    @import '@/assets/electro/css/style.css';
+    @import 'bootstrap/dist/css/bootstrap.min.css';
+   
 
+
+    
 </style>
